0525c65435844f7e754d38a8a6d238fa5cbf9434
gesparza3 <gesparza3@mail.csuchico.edu>
2018-09-12 13:48:11 -0700

    Added bootstrap notes and confidence interval example

--- in_class/bootstrap.R
+++ in_class/bootstrap.R
@@ -18,3 +18,48 @@ idx <- sample(1:100, 10)
 
 idx[c(1, 2, 5, 6)] ## Example vector index
 idx[sample(1:100, 10)] ## Another example
+
+###############################################################################
+
+## Bootstrap stuff
+hospitals <- read.csv("https://roualdes.us/data/hospital.csv")
+T <- 999
+xbars <- rep(NA, T)
+for (t in 1:T) {
+    data <- hospitals$infection_risk
+    N <- length(data)
+    idx <- sample(1:N, size=N, replace=TRUE)
+    xbars[t] <- mean(data[idx])
+}
+
+ste <- sd(xbars) ## Standard error
+
+## Confidence interval
+mean(data) - 1.96*ste ## lower bound
+mean(data) + 1.96*ste ## upper bound
+
+# We are 95% confident that the true population mean infection_risk is between 4.101 and 4.608.
+
+#################################################################################
+
+## Practice on your own
+T <- 1500
+xbars <- rep(NA, T)
+for (t in 1:T) {
+    data <- hospitals$beds
+    N <- length(data)
+    idx <- sample(1:N, size=N, replace=TRUE)
+    xbars[t] <- mean(data[idx])
+}
+
+ste <- sd(xbars)
+# [1] 17.47625
+
+# Confidence interval
+mean(data) - 1.96*ste ## lower bound
+# [1] 216.7782
+mean(data) + 1.96*ste ## upper bound
+# [1] 287.5581
+
+# We are 95% confident that the true population mean number of beds is between 216.78 and 287.6 beds.
+
