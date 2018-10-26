e71d90275bb099dee9871b99656717bf4fdf79b6
gesparza3 <gesparza3@mail.csuchico.edu>
2018-10-26 12:52:20 -0700

    Finished webscraping2 assignment

--- homework/webscraping2/webscrape2.R
+++ homework/webscraping2/webscrape2.R
@@ -6,9 +6,10 @@ library(httr)
 library(knitr)
 library(dplyr)
 library(ggplot2)
+library(ggExtra)
 library(stringr)
 library(RSQLite)
-library(gridExtra)
+library(lubridate)
 library(data.table)
 
 
@@ -50,6 +51,20 @@ for (link in complete_links[4:14]) {
 
 files <- list.files(path="aqi", full.names=TRUE)
 aqi.df <- rbindlist(lapply(files, fread))
+colnames(aqi.df) <- c("region", "subregion", "state.code", "county.code", "date",
+                      "aqi", "category", "defining.parameter", "defining.site",
+                      "number.of.sites.reporting")
+aqi.df$date <- year(aqi.df$date)
+
+################################################################################
+# Generate AQI statistics
+################################################################################
+
+## Get a tibble
+montana.aqi <- aqi.df %>%
+  filter(region == "Montana") %>%
+  group_by(date, subregion) %>%
+  summarise(mn_aqi=mean(aqi), md_aqi=median(aqi))
 
 ################################################################################
 # Read wildfire data
@@ -58,75 +73,73 @@ aqi.df <- rbindlist(lapply(files, fread))
 ## Extract fire table from database
 db <-dbConnect(SQLite(), "fire_database.sqlite")
 res <- dbSendQuery(db, "SELECT * FROM Fires WHERE State == 'MT'")
-montana_fires <- dbFetch(res)
+fires <- dbFetch(res)
 
 ################################################################################
-# Generate statistics
+# Generate fire statistics
 ################################################################################
 
 ## Get a tibble
-montana_fires %>%
-    group_by(FIPS_NAME) %>%
-    summarise(n=n(), mn_size=mean(FIRE_SIZE), sum_size=sum(FIRE_SIZE)) -> num_fires
-  num_fires <- num_fires[1:56,]
+montana.fires <- fires %>%
+    group_by(FIRE_YEAR, FIPS_NAME) %>%
+    summarise(num_fires=n(), mn_fire_size=mean(FIRE_SIZE),
+              sum_fire_size=sum(FIRE_SIZE))
 
 ################################################################################
-# Plot fire data
+# Join data
 ################################################################################
 
-## Map plot
-counties <- map_data("county") %>% filter(region == "montana")
+## Rename columns for join
+colnames(montana.fires)[colnames(montana.fires) == "FIRE_YEAR"] <- "date"
+colnames(montana.fires)[colnames(montana.fires) == "FIPS_NAME"] <- "subregion"
 
-## Fix county names
-colnames(num_fires) <- c("subregion", "num_fires", "mn_fire_size", "sum_fire_size")
+## Join
+montana.comp <- montana.aqi %>%
+  inner_join(montana.fires, by=c("subregion", "date"))
 
-## Fix subregion names
-num_fires$subregion <- tolower(num_fires$subregion)
-
-## Plot
-montana_map <- ggplot(data=counties, aes(x=long, y=lat, group=group)) +
-    coord_fixed(1.3) +
-    geom_polygon(color="black", fill="gray")
-
-## Join datasets
-fire_map <- inner_join(counties, num_fires, by="subregion")
-
-## Plot map with data
-fire_base_map <- montana_map +
-    geom_polygon(data=fire_map, aes(fill=num_fires), color="black") +
-    ggtitle("Total Acres Burned by County 1992-2015") +
-    scale_fill_gradient2(trans="log10", low="#FFFFE0", mid="#FEB24C", high="#CD0000") +
-    labs(fill="Total Acres Burned") +
-    theme_void() +
-    theme(title = element_text(face="bold"))
+## Switch case
+montana.comp$subregion <- tolower(montana.comp$subregion)
 
 ################################################################################
-# Plot aqi data
+# Plot map of AQI
 ################################################################################
 
-counties <- map_data("county")
+## Get county data
+counties <- map_data("county") %>%
+  filter(region == "montana")
 
-## Fix aqi names
-colnames(aqi.df) <- c("region", "subregion", "state.code", "county.code", "date",
-                      "aqi", "category", "defining.parameter", "defining.site",
-                      "number.of.sites.reporting")
-aqi.df$region <- tolower(aqi.df$region)
-aqi.df$subregion <- tolower(aqi.df$subregion)
+## Join with montana data
+geo.dat <- montana.comp %>% inner_join(counties, by="subregion")
 
-## Select montana
-montana.dat <- aqi.df %>% filter(region == "montana")
-
-## Generate mean aqi per county
-aqi.county <- aqi.df %>%
-  group_by(subregion) %>%
-  summarise(mn_aqi=mean(aqi), md_aqi=median(aqi))
-
-## Join stat data
-aqi.map <- inner_join(counties, aqi.county, by="subregion")
-
-ggplot(data=counties[counties$region == "montana",], aes(x=long, y=lat, group=group)) +
+## Plot US map with data
+ggplot(data=counties, aes(x=long, y=lat, group=group)) +
     coord_fixed(1.3) +
     geom_polygon(color="black", fill="gray") +
-    geom_polygon(data=aqi.county, aes(fill=mn_aqi), color="black") +
+    geom_polygon(data=geo.dat, aes(fill=mn_aqi), color="black") +
     scale_fill_gradient2(low="#FFFFE0", mid="#FEB24C", high="#CD0000") +
     theme_void()
+
+################################################################################
+# Plot the data
+################################################################################
+
+## Select western counties
+montana.western <- montana.comp %>%
+  filter(subregion %in% c("Lincoln", "Flathead", "Sanders", "Lake", "Missoula",
+                        "Ravalli", "Powell"))
+
+## Number of fires explains mean_aqi
+p <- ggplot(montana.western, aes(log(num_fires), log(mn_aqi))) +
+  geom_point(aes(color=subregion)) +
+  stat_smooth(method="lm", se=FALSE)
+
+## Add margin plots
+ggMarginal(p, type="boxplot")
+
+################################################################################
+# Create a model
+################################################################################
+
+summary(lm(log(mn_aqi)~log(num_fires), data=montana.western))
+
+
